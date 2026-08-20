6c9bb5f9f6080ae42e6a5a895d9cc032a7070b5f
shahdreda358-ui <shahdreda358@gmail.com>
2026-08-20 04:38:48 +0300

    Add files via upload

---
+++ Scripts/memories.js
@@ -0,0 +1,403 @@
+const memories = {
+
+    1: {
+        title: "ذكرياتنا ❤️",
+
+        photos: [
+            {
+                image: "./Images/eid at 3.04.07 PM.jpeg",
+                text: " كل عيد وانتي عيدي و عيديتي ♥️❤️"
+            },
+            {
+                image: "./Images/eid2.jpeg",
+                text: "تاني عيد والوليه بعيد 😐♥️🥹"
+            },
+            // {
+            //     image: "./Images/photo3.jpg",
+            //     text: "كل صورة هنا وراها حكاية ❤️"
+            // },
+            // {
+            //     image: "./Images/photo4.jpg",
+            //     text: "وأجمل حاجة إنك كنت معايا فيها 🤍"
+            // }
+        ]
+    },
+
+
+    2: {
+        title: "ببص لأهدافي 👀🎯❤️",
+
+        photos: [
+            {
+                image: "./Images/goal-1.jpeg",
+                text: "اول يوم ليكي ف الحجاب ساعتها انا كنت زي الطفل الصغير من كتر الفرحه واظن عيني الهبله كانت بتتكلم ♥️😂"
+            },
+            {
+                image: "./Images/goal-2.jpeg",
+                text: "اليوم دا كان حر اوي اوي اوي واللي هوا عاوز اقتلك علشان منزلاني ف الحر انا اساسا بتلكك علشان اخرج معاكي😂♥️♥️♥️"
+            },
+            {
+                image: "./Images/Birthday.jpeg",
+                text: "اول مره ف حياتي يتعملي عيد ميلاد كان ع ايدك انتي بحبك اوي اوي اوي والله بجد شكرا علي كل لحظه وكل مجهود عملتيه اليوم دا بجد بحبك ♥️♥️♥️♥️♥️♥️♥️♥️♥️"
+            },
+            {
+                image: "./Images/quiz.jpeg",
+                text: "اخر يوم امتحانات الترم التاني نظرتي ليكي وانا فخور بكل لحظه بتعدي من عمري وانا معاكي وشايفك بتكبري ♥️♥️"
+            }
+            , {
+                image: "./Images/part-2.jpeg",
+                text: " ربنا يديمها ♥️"
+            },
+            {
+                image: "./Images/goal-3.jpeg",
+                text: "بصراحة ببص لأهدافي انتي أهدافي ♥️"
+            },
+            {
+                image: "./Images/Masterpiece ✨.jpeg",
+                text: " Masterpiece ✨"
+            }
+            , {
+                image: "./Images/perfect.jpeg",
+                text: " الصورة دي بحبها اوي اوي ولحظتها معايا حلوة اوي والله  شكلك اليوم دة بيرفيكتو ♥️"
+            }
+
+        ]
+    },
+
+
+    3: {
+        title: "أنت وأنا ❤️",
+
+        photos: [
+            {
+                image: "./Images/imgg1.jpeg",
+                text: "تاني مره نروح سينما واليوم دة كان مووووت 😂♥️♥️♥️"
+            },
+            {
+                image: "./Images/imgg2.jpeg",
+                text: "اخر خروجه لينا ف المعادي مش هقولك بقا حاجات كتيره غير إن الصورة دي انا الدنيا كلها ب فرحتها متجيش جمبي حاجة دي معناها الأمان وكفي ♥️"
+            },
+            {
+                image: "./Images/imgg3.jpeg",
+                text: "اني روف نروحه وبصراحه مش هوصف انبسطنا اليوم دة كان عامل ازاي بس اللي اقدر اقوله ان لسه باطني وجعاني لحد دلوقتي من كتر الضحك ف اليوم دة ♥️😂😂"
+            },
+            {
+                image: "./Images/special4.jpeg",
+                text: "اول يوم نروح سينما ونخش the mummy  عليكي انتي كويسه من ساعه الفيلم ده 😂♥️♥️"
+            },
+            {
+                image: "./Images/quiz2.jpeg",
+                text: " اخر يوم امتحانات الترم الاول وطبعا دي لحظه تتوثق عشان كدة كدة كل اللحظات اللي جايه هكون جمبك ♥️"
+            },
+            {
+                image: "./Images/special5.jpeg",
+                text: "اول خروجه لينا ف روف و كتجربه بصراحه اديها 100  من10 ال 100 ليكي ب المناسبه عشان انتي التوب 😂♥️"
+            }
+
+        ]
+    },
+
+
+    4: {
+        title: "لحظات مميزة 💗",
+
+        photos: [
+            {
+                image: "./Images/special1.jpeg",
+                text: "يعني مش هتكلم انا هسيب الصورة تحكي ♥️"
+            },
+            {
+                image: "./Images/special2.jpeg",
+                text: "مفيش حد مش مبسوط كله فرحان لينا واحنا كنا اعلي مراحل الفرحه عندنا والله ♥️"
+            },
+            {
+                image: "./Images/special3.jpeg",
+                text: "الزوج & الزوجه ♥️"
+            },
+
+
+        ]
+    },
+
+
+    5: {
+        title: "ذكريات مش هتتنسي ❤️",
+
+        photos: [
+            {
+                image: "./Images/memorie1.jpeg",
+                text: "اول صورة تجمعنا سوا ♥️"
+            },
+            {
+                image: "./Images/memorie2.jpeg",
+                text: "اكتر يوم بحبه يوم م روحنا اسكندريه سوااا وقولتلك هناك اني جااااااااي لابوكي 🫡♥️"
+            },
+            {
+                image: "./Images/memorie3.jpeg",
+                text: "خروجه رمضان واليوم دة بجد كان جميل اوي بكل تفاصيله وطبعا اهم حاجة فيه اننا سوا بحبك اوي اوي ♥️♥️"
+            }
+        ]
+    },
+
+
+    6: {
+        title: "حبيبه قلبي و روحي❤️",
+
+        photos: [
+            {
+                image: "./Images/foryou.jpeg",
+                text: "اجمل واحلي صورة صورتهالك ف حياتي بحبك اوي اوي اوي ♥️ سنه وانتي العمر وكل سنه وانتي حياتي وكل سنه وانتي دنيتي كلها ♥️♥️ سنه واحنا مع بعض لآخر العمر ي اجمل صدفه ف العمر ♥️"
+            },
+            {
+                image: "./Images/bnti.jpeg",
+                text: "بنتي 😂♥️♥️❤️"
+            },
+            
+        ]
+    },
+    7: {
+        title: " ♥️😂العقل زينه والله",
+
+        photos: [
+            {
+                image: "./Images/111.jpeg",
+                text: "هبل هبل هبل بس بحبه ♥️♥️♥️😂😂"
+            },
+            {
+                image: "./Images/333.jpeg",
+                text: "هبل هبل هبل تاني part 2 😂♥️"
+            },
+            {
+                image: "./Images/444.jpeg",
+                text: "هبل هبل هبللللل part 3"
+            }
+        ]
+    },
+    8: {
+        title: " ♥️😂اماني ومامني ",
+
+        photos: [
+            {
+                image: "./Images/dafa1.jpeg",
+                text: "القصيره حجي 😂♥️"
+            },
+            {
+                image: "./Images/777.jpeg",
+                text: " ي ديني ي جدعان دانا محظوظ اوي والله ♥️"
+            },
+            {
+                image: "./Images/888.jpeg",
+                text: "الجمال كله = انتي ♥️"
+            }
+        ]
+    }
+}
+
+// =========================
+// Current Memory
+// =========================
+
+let currentMemory = null;
+
+let currentPhotoIndex = 0;
+
+
+// =========================
+// Open Memory
+// =========================
+
+function openMemory(number) {
+
+    currentMemory = memories[number];
+
+    currentPhotoIndex = 0;
+
+
+    document.getElementById("memoryTitle").textContent =
+        currentMemory.title;
+
+
+    document.getElementById("memoryModal").style.display =
+        "flex";
+
+
+    showPhoto();
+}
+
+
+// =========================
+// Show Photo
+// =========================
+
+function showPhoto() {
+
+    if (!currentMemory) return;
+
+
+    const photo =
+        currentMemory.photos[currentPhotoIndex];
+
+
+    const image =
+        document.getElementById("currentPhoto");
+
+
+    // وضع الصورة
+    image.src = photo.image;
+
+
+    // الكلام اللي هيظهر على ضهر الصورة
+    const backText =
+        document.getElementById("photoBackText");
+
+
+    if (backText) {
+
+        backText.textContent =
+            photo.text;
+    }
+
+
+    // رقم الصورة
+
+    const counter =
+        document.getElementById("photoCounter");
+
+
+    if (counter) {
+
+        counter.textContent =
+            `${currentPhotoIndex + 1} / ${currentMemory.photos.length}`;
+    }
+
+
+    // =========================
+    // رجّع الصورة للوش
+    // =========================
+
+    const flipInner =
+        document.querySelector(".flip-inner");
+
+
+    if (flipInner) {
+
+        flipInner.classList.remove("flipped");
+    }
+
+
+    // =========================
+    // Previous Button
+    // =========================
+
+    const prevButton =
+        document.querySelector(".prev-btn");
+
+
+    if (prevButton) {
+
+        prevButton.disabled =
+            currentPhotoIndex === 0;
+    }
+
+
+    // =========================
+    // Next Button
+    // =========================
+
+    const nextButton =
+        document.querySelector(".next-btn");
+
+
+    if (nextButton) {
+
+        nextButton.disabled =
+            currentPhotoIndex ===
+            currentMemory.photos.length - 1;
+    }
+
+}
+
+
+// =========================
+// Flip Photo
+// Double Click
+// =========================
+
+function flipPhoto() {
+
+    const flipInner =
+        document.querySelector(".flip-inner");
+
+
+    if (flipInner) {
+
+        flipInner.classList.toggle("flipped");
+    }
+
+}
+
+
+// =========================
+// Next Photo
+// =========================
+
+function nextPhoto() {
+
+    if (!currentMemory) return;
+
+
+    if (
+        currentPhotoIndex <
+        currentMemory.photos.length - 1
+    ) {
+
+        currentPhotoIndex++;
+
+        showPhoto();
+    }
+
+}
+
+
+// =========================
+// Previous Photo
+// =========================
+
+function previousPhoto() {
+
+    if (!currentMemory) return;
+
+
+    if (currentPhotoIndex > 0) {
+
+        currentPhotoIndex--;
+
+        showPhoto();
+    }
+
+}
+
+
+// =========================
+// Close Memory
+// =========================
+
+function closeMemory() {
+
+    document.getElementById("memoryModal").style.display =
+        "none";
+
+
+    // لما تقفلي النافذة
+    // رجّعي الكارت للوش
+
+    const flipInner =
+        document.querySelector(".flip-inner");
+
+
+    if (flipInner) {
+
+        flipInner.classList.remove("flipped");
+    }
+
+}
